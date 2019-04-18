18e630acbf62ca342aec83ee65608cddce122f1e
Kiran <kirankx4007@gmail.com>
2019-04-18 17:17:53 +0530

    Updated UI and changed delay mechanism

--- app/src/main/java/com/example/freak/ltx/MainActivity.java
+++ app/src/main/java/com/example/freak/ltx/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.freak.ltx;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
@@ -13,8 +14,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,10 +25,11 @@ public class MainActivity extends AppCompatActivity {
     RadioButton spd;
     Button send;
     EditText out,in;
+    Long start_time,temp_t,fin_time;
+    TextView t1;
+    ProgressDialog diag;
     static final int CAMERA_REQUEST=50;
     boolean Camperm;
-    Handler handler;
-    Runnable runnable;
     String fin_data,temp_data;
     int in_count,flash_status;
     Integer speed;
@@ -37,36 +41,16 @@ public class MainActivity extends AppCompatActivity {
         send=findViewById(R.id.button);
         in=findViewById(R.id.editText);
         out=findViewById(R.id.editText2);
+        t1=findViewById(R.id.textView3);
+        diag= new ProgressDialog(MainActivity.this);
+        fin_time=0L;
+        diag.setMessage("Sending.....");
         Camperm=ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 == PackageManager.PERMISSION_GRANTED;
         in_count=0;
         flash_status=0;
         temp_data="";
-        handler= new Handler();
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-             if(in_count<fin_data.length()){
-             char ch=fin_data.charAt(in_count);
-             if(ch=='1'&& flash_status==0){
-                 flashLightOn();
-                 flash_status=1;}
-             else if(ch=='0'){
-                 flash_status=0;
-                 flashLightOff();}
-             in_count++;
-             temp_data=temp_data+ch;
-             out.setText(temp_data);
-             // DEBUG PURPOSE 50ms is considered as 1sec to visuvalize
-             if(speed==50)
-                 speed=1000;
-             handler.postDelayed(runnable,speed);
-             }
-             else
-                 send.setEnabled(true);
 
-            }
-        };
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -81,13 +65,33 @@ public class MainActivity extends AppCompatActivity {
                     String temp= spd.getText().toString();
                     // Getting tx speed
                     if(temp.length()>3)
-                    speed=Integer.valueOf(temp.substring(0,2));
+                    speed=25;
                     else
-                        speed=Integer.valueOf(temp.substring(0,1));
+                        speed=1000;
                     if (in.getText().toString().isEmpty()) {
                         Toast.makeText(MainActivity.this, "Enter Data", Toast.LENGTH_SHORT).show();
-                    } else
-                        transmit();
+                    } else{
+                        diag.show();
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try{transmit();}
+                                catch (Exception e) {
+                                e.printStackTrace();
+                                }
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        out.setText(fin_data);
+                                        t1.setText(Long.toString(fin_time));
+                                    }
+                                });
+                                diag.dismiss();
+                            }
+
+                        }).start();
+
+                        }
                 }
             }
         });
@@ -115,7 +119,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
         protected void transmit(){
-        send.setEnabled(false);
         in_count=0;
         temp_data="";
         fin_data="";
@@ -132,7 +135,25 @@ public class MainActivity extends AppCompatActivity {
             dat_byte="1"+dat_byte+"0";
             fin_data=fin_data+dat_byte;
         }
-        handler.postDelayed(runnable,0);
+        send_dat();
+
+    }
+
+    protected void send_dat() {
+        temp_t=System.currentTimeMillis();
+        while(in_count<fin_data.length()){
+            start_time=System.currentTimeMillis();
+            char ch=fin_data.charAt(in_count);
+            if(ch=='1'&& flash_status==0){
+                flashLightOn();
+                flash_status=1;}
+            else if(ch=='0'){
+                flash_status=0;
+                flashLightOff();}
+            in_count++;
+            while(System.currentTimeMillis()-start_time<speed);
+        }
+        fin_time=System.currentTimeMillis()-temp_t;
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions,  int[] grantResults) {
